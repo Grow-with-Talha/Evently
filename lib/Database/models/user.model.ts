da543a41853594414d3860c4d96ffc23ef9fa26b
Talha Ali <alit83219@gmail.com>
2024-01-05 13:14:15 +0500

    hello

--- lib/Database/models/user.model.ts
+++ lib/Database/models/user.model.ts
@@ -3,7 +3,7 @@ import { Schema, model, models } from "mongoose";
 const userSchema = new Schema({
     clerkID: {type: String, required: true, unique: true},
     email: {type: String, required: true, unique: true},
-    userName: {type: String, required: true, unique: true},
+    username: {type: String, required: true, unique: true},
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
     photo: {type: String, required: true},
