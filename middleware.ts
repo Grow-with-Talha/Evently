88157c3fe4d9a0944cb4dbeb44cd3556fa58b6bb
Talha Ali <alit83219@gmail.com>
2024-01-12 10:29:44 +0500

    aisay hij

--- middleware.ts
+++ middleware.ts
@@ -10,7 +10,7 @@ export default authMiddleware({
     "/api/webhook/clerk",
     "/api/webhook/stripe",
     "/api/uploadthing",
-    "/api/webhooks(.*)",
+    "/api/webhook(.*)",
   ],
   ignoredRoutes: [
     "/api/webhook/clerk",
