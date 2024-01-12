aae34dd0c2ed47577ec75bb199334336cc4c8d7d
Talha Ali <alit83219@gmail.com>
2024-01-12 10:29:20 +0500

    editesd middleware

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,25 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
+
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-    publicRoutes: [
-        "/",
-        "/events/:id",
-        "/api/webhook/clerk",
-        "/api/webhook/stripe",
-        "/api/uploadthing",
-    ],
-    ignoredRoutes: [
-        "/api/webhook/clerk",
-        "/api/webhook/stripe",
-        "/api/uploadthing",
-    ]
+  publicRoutes: [
+    "/",
+    "/events/:id",
+    "/api/webhook/clerk",
+    "/api/webhook/stripe",
+    "/api/uploadthing",
+    "/api/webhooks(.*)",
+  ],
+  ignoredRoutes: [
+    "/api/webhook/clerk",
+    "/api/webhook/stripe",
+    "/api/uploadthing",
+    "/api/webhook(.*)",
+  ],
 });
- 
+
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
- 
