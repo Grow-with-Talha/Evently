f451edc894273badd1427d0b5db7962d9cd102e1
Talha Ali <alit83219@gmail.com>
2024-01-13 09:26:00 +0500

    alkdjf

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,70 +1,81 @@
-'use server'
+"use server";
 
-import { revalidatePath } from 'next/cache'
+import { revalidatePath } from "next/cache";
 
-import {  connectToDB } from '@/lib/Database'
-import User from '@/lib/Database/models/user.model'
-import Order from '@/lib/Database/models/order.model'
-import Event from '@/lib/Database/models/event.model'
-import { handleError } from '@/lib/utils'
+import { connectToDB } from "@/lib/Database";
+import User from "@/lib/Database/models/user.model";
+import Order from "@/lib/Database/models/order.model";
+import Event from "@/lib/Database/models/event.model";
+import { handleError } from "@/lib/utils";
 
-import { CreateUserParams, UpdateUserParams } from '@/types'
+import { CreateUserParams, UpdateUserParams } from "@/types";
 
 export async function createUser({
-  clerkId, username, firstName, lastName, photo, email
+  clerkId,
+  username,
+  firstName,
+  lastName,
+  photo,
+  email,
 }: CreateUserParams) {
   try {
-    await connectToDB()
-    console.log("from create user",clerkId)
-    const newUser = await User.create({
-      clerkId: clerkId,
-      username: username, 
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      photo: photo
-    })
-    return JSON.parse(JSON.stringify(newUser))
+    await connectToDB();
+    console.log("from create user", clerkId);
+    if (clerkId === null) {
+      return console.log("clerkid is null");
+    } else {
+      const newUser = await User.create({
+        clerkId: clerkId,
+        username: username,
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        photo: photo,
+      });
+      return JSON.parse(JSON.stringify(newUser));
+    }
   } catch (error) {
-    handleError(error)
+    handleError(error);
   }
 }
 
 export async function getUserById(userId: string) {
   try {
-    await connectToDB()
+    await connectToDB();
 
-    const user = await User.findById(userId)
+    const user = await User.findById(userId);
 
-    if (!user) throw new Error('User not found')
-    return JSON.parse(JSON.stringify(user))
+    if (!user) throw new Error("User not found");
+    return JSON.parse(JSON.stringify(user));
   } catch (error) {
-    handleError(error)
+    handleError(error);
   }
 }
 
 export async function updateUser(clerkId: string, user: UpdateUserParams) {
   try {
-    await connectToDB()
+    await connectToDB();
 
-    const updatedUser = await User.findOneAndUpdate({ clerkId }, user, { new: true })
+    const updatedUser = await User.findOneAndUpdate({ clerkId }, user, {
+      new: true,
+    });
 
-    if (!updatedUser) throw new Error('User update failed')
-    return JSON.parse(JSON.stringify(updatedUser))
+    if (!updatedUser) throw new Error("User update failed");
+    return JSON.parse(JSON.stringify(updatedUser));
   } catch (error) {
-    handleError(error)
+    handleError(error);
   }
 }
 
 export async function deleteUser(clerkId: string) {
   try {
-    await connectToDB()
+    await connectToDB();
 
     // Find user to delete
-    const userToDelete = await User.findOne({ clerkId })
+    const userToDelete = await User.findOne({ clerkId });
 
     if (!userToDelete) {
-      throw new Error('User not found')
+      throw new Error("User not found");
     }
 
     // Unlink relationships
@@ -76,15 +87,18 @@ export async function deleteUser(clerkId: string) {
       ),
 
       // Update the 'orders' collection to remove references to the user
-      Order.updateMany({ _id: { $in: userToDelete.orders } }, { $unset: { buyer: 1 } }),
-    ])
+      Order.updateMany(
+        { _id: { $in: userToDelete.orders } },
+        { $unset: { buyer: 1 } }
+      ),
+    ]);
 
     // Delete user
-    const deletedUser = await User.findByIdAndDelete(userToDelete._id)
-    revalidatePath('/')
+    const deletedUser = await User.findByIdAndDelete(userToDelete._id);
+    revalidatePath("/");
 
-    return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null
+    return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null;
   } catch (error) {
-    handleError(error)
+    handleError(error);
   }
 }
